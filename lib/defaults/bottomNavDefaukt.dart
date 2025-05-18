d843ab5d321b01b2cdded4f7a44197b9d546afc0
Ademola Victor Oluokun <137506855+Adevictorolu@users.noreply.github.com>
2025-05-18 04:01:31 +0100

    added

--- lib/defaults/bottomNavDefaukt.dart
+++ lib/defaults/bottomNavDefaukt.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class BottomNavDefault {
   static const Color bottomNavitemcolor = Colors.white;
-  static const Color? buttomNavitemselectedcolor = Colors.black;
+  static const Color buttomNavitemselectedcolor = Colors.black;
   static final bottomNavitemtext = ['Inbox', 'Starred', 'Sent', 'Draft', 'Trash', 'Spam'];
   static final bottomNavitemicon = [Icons.inbox, Icons.star, Icons.send, Icons.mail, Icons.delete, Icons.warning_rounded];
 }
