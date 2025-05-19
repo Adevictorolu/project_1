f578e12b82aee87c5eff000686c2002517b28356
Ademola Victor Oluokun <137506855+Adevictorolu@users.noreply.github.com>
2025-05-19 19:13:43 +0100

    ADDED SUCCESSFULLY

--- lib/bottomNav.dart
+++ lib/bottomNav.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:project_1/defaults/bottomNavDefaukt.dart';
+import 'package:project_1/defaults/bottomnavdefault.dart';
 
 class ButtomNavScreen extends StatefulWidget {
   const ButtomNavScreen({super.key});
